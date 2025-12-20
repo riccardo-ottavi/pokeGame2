4883764e06f945ed48d068a7b9e953deb516d4a6
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-20 14:22:45 +0100

    inizializza logica inizializzazione gioco

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import { useState } from 'react';
 
 function App() {
- 
+
   //----------stati---------------
-  const [ stage, setStage ] = useState(0)
-  const [ player, setPlayer ] = useState({})
-  const [ enemy, setEnemy ] = useState({})
-  const [ isGameOver, setIsGameOver ] = useState(false)
+  const [stage, setStage] = useState(0)
+  const [player, setPlayer] = useState({})
+  const [enemy, setEnemy] = useState({})
+  const [isGameOver, setIsGameOver] = useState(false)
 
   //----------costanti---------------
   const baseUrl = "https://pokeapi.co/api/v2/"
@@ -19,12 +19,18 @@ function App() {
 
   //inizializza classe per istanziare i pokemon effettivamente in gioco
   class PokemonInstance {
-    
+    constructor(id, level, maxHp, currentHp, status) {
+      this.id = id;
+      this.level = level;
+      this.maxHp = maxHp;
+      this.currentHp = currentHp;
+      this.status = status;
+    }
   }
 
   //gestisce la logica dei calcoli relativi alle statistiche
   class Stats {
-    
+
   }
 
   //gestisce la logica delle mosse(tipo, fallimento/critico, calcolo esito in danni)
@@ -33,57 +39,70 @@ function App() {
   }
 
   //gestisce la logica degli oggetti
-  class Items{
+  class Items {
 
   }
 
   //gestisce andamento del gioco
-  class Progression{
+  class Progression {
 
   }
 
   //----------inizializzazioni---------------
 
-  function runGame(){
+  function runGame() {
     console.log("il gioco è iniziato");
-    instancePokemon();
+    instancePokemon("player");
+    instancePokemon("enemy");
     handleProgression();
   }
 
   //inizializza player e nemico con this. e gli aggiunge proprietà items e moveset
-  function instancePokemon(fetchedPokemon){
-    console.log("hai chiamato l'inizializzazione dei pokemon (player e nemico)");
+  async function instancePokemon(target) {
+    //capisci se stai impostando player o enemy e assegna pokemon fetchato allo stato 
+    console.log("hai chiamato l'inizializzazione dei pokemon di", target);
+    if(target === "player"){
+      const poke = await fetchFromApi("pokemon", generateRandomId(idLimit));
+      setPlayer(poke)
+      
+    }else{
+      const poke = await fetchFromApi("pokemon", generateRandomId(idLimit));
+      setEnemy(poke)
+    }
+    
+    console.log(player, enemy)
     initializeMoveset();
     initializeItems();
   }
 
- //fetcha qualcosa
-  async function fetchFromApi(category, id){
-    const requestedPromise = await fetch(`${baseUrl}${category}/${id}`)
-    const requestedObj = await requestedPromise.json()
-    return requestedObj
+  
+
+  //fetcha qualcosa
+  async function fetchFromApi(category, id) {
+    const requestedPromise = await fetchJson(`${baseUrl}${category}/${id}`)
+    return requestedPromise
   }
 
   //inizializza il moveset(andrà fatta una chiamata a /moves)
-  function initializeMoveset(poke){
+  function initializeMoveset(poke) {
 
   }
 
   //inizializza il moveset(andrà fatta una chiamata a /items)
-  function initializeItems(){
+  function initializeItems() {
 
   }
 
   //------------fight system-------------
-  function handleFight(player, enemy){
-    
+  function handleFight(player, enemy) {
+
   }
 
-  function chechWhoFaster(player, enemy){
-    
+  function chechWhoFaster(player, enemy) {
+
   }
 
-  function useItem(item){
+  function useItem(item) {
 
   }
 
@@ -91,29 +110,36 @@ function App() {
   //----------progressione---------------
 
   //genera la progressione in rapporto agli stage 
-  function handleProgression(){
+  function handleProgression() {
     console.log("avvio progressione in base allo stage. STAGE: ", stage)
   }
 
-  function generateNewFight(stage){
+  function generateNewFight(stage) {
 
   }
 
-  function handleReward(stage){
+  function handleReward(stage) {
 
   }
 
   //----------utilities---------------
   // id casuale per randomizzare da 0 a un limite 
-  function generateRandomId(max){
+  function generateRandomId(max) {
     const random = Math.round(Math.random() * max)
     return random
   }
 
+  //parsa le response in obj JSON
+  async function fetchJson(url) {
+    const response = await fetch(url);
+    const obj = await response.json();
+    return obj
+  }
+
 
   //main
-  runGame()
- 
+  //runGame()
+  //fetchFromApi("pokemon",1).then(p => console.log(p))
 
   return (
     <>
